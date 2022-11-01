1f8e6bc835725ef5de8d34da4290f298e12c0f56
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-11-01 20:30:21 +0300

    copy random lines from the given files to the output txt

--- myprog2.sh
+++ myprog2.sh
@@ -1,9 +1,27 @@
+#!/bin/bash
+
 ## general input check
 if [ $# -eq 0 ]
 then
 echo "$0 : You must give/supply 1 file name with a txt extension"
 exit 1
 fi
+
+## non-empty lines array
+lines=(1 3 5)
+
+# seed random
+RANDOM=$$$(date +%s)
+
+
+randomLine1=${lines[$(($RANDOM % ${#lines[@]}))]}
+randomLine2=${lines[$(($RANDOM % ${#lines[@]}))]}
+randomLine3=${lines[$(($RANDOM % ${#lines[@]}))]}
+
+## added for debug purposes remove them before merging
+echo "$randomLine1"
+echo "$randomLine2"
+echo "$randomLine3"
 
 ## check if the file exists
 if [ -e $1 ] 
@@ -12,22 +30,22 @@ then
   read answer
   if [ $answer = "Y" -o $answer = "y" ]
   then
-  	 sed -n '5,6p' giris.txt > $1
-  	 sed -n '1,2p' gelisme.txt >> $1
-  	 sed -n '3,4p' sonuc.txt >> $1
+  	 sed -n ''"$randomLine1"','"$((randomLine1+1))"'p' giris.txt > $1
+  	 sed -n ''"$randomLine2"','"$((randomLine2+1))"'p' gelisme.txt >> $1
+  	 sed -n ''"$randomLine3"','"$((randomLine3+1))"'p' sonuc.txt >> $1
   	
   elif	[ $answer = "N" -o $answer = "n" ]
   then
-  	sed -n '5,6p' giris.txt >> $1
-  	sed -n '1,2p' gelisme.txt >> $1
-  	sed -n '3,4p' sonuc.txt >> $1
+  	sed -n ''"$randomLine1"','"$((randomLine1+1))"'p' giris.txt >> $1
+  	sed -n ''"$randomLine2"','"$((randomLine2+1))"'p' gelisme.txt >> $1
+  	sed -n ''"$randomLine3"','"$((randomLine3+1))"'p' sonuc.txt >> $1
   else
   	echo "please enter either y/n"
   fi		
 else
-  	sed -n '5,6p' giris.txt > $1
-  	sed -n '1,2p' gelisme.txt >> $1
-  	sed -n '3,4p' sonuc.txt >> $1
+  	sed -n ''"$randomLine1"','"$((randomLine1+1))"'p' giris.txt > $1
+  	sed -n ''"$randomLine2"','"$((randomLine2+1))"'p' gelisme.txt >> $1
+  	sed -n ''"$randomLine3"','"$((randomLine3+1))"'p' sonuc.txt >> $1
 fi
 
 
